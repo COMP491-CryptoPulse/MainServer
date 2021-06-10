51298d282543bffaba3567f8b244193ad152f707
utkn <39ph4f87a@gmail.com>
2021-06-10 19:24:34 +0300

    Frontend update.

--- web/out/_next/static/chunks/webpack.js
+++ web/out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d2ccb0e51d1c568bc45b";
+/******/ 	var hotCurrentHash = "ef80d7d45837bb839231";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
